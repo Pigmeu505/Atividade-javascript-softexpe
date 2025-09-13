05251443502a955d697090745999c3653fc7a21e
Renan Ferro <rickferro505@gmail.com>
2025-09-13 08:58:54 -0300

    Delete ex11.js

--- ex11.js
+++
@@ -1,9 +0,0 @@
-n1 = parseInt(prompt("Digite um numero:"))
-
-meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
-
-if (n1 <= 12){
-    alert(meses[n1-1])
-} else {
-    alert("Numero invalido, escreva entre 1 e 12")
-}
