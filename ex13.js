b5c0bdabb9f8cf49f2c7b9e529676546b98386a2
Renan Ferro <rickferro505@gmail.com>
2025-09-13 08:59:09 -0300

    Delete ex13.js

--- ex13.js
+++
@@ -1,7 +0,0 @@
-n = parseInt(prompt("Digite um número para ver a tabuada:"))
-console.log("Tabuada do " + n + ":")
-x = 1
-while (x <= 10) {
-    console.log(n + " x " + x + " = " + (n * x));
-    x++
-}
